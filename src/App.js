511650697d878de24347926bf149f6d1475d9f22
thejaduijin <lokendra.06.work@gmail.com>
2022-11-28 18:14:34 +0530

    Api added for future

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+// import React, { useEffect } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Blog from './pages/Blog/Blog.jsx';
 import Home from './pages/Home/Home.jsx';
+// import axios from "axios";
 
 const App = () => {
+  // const fetchNews = async () => {
+  //   const { data } = await axios.get(
+  //     `https://newsapi.org/v2/everything?q=apple&from=2022-11-27&to=2022-11-27&sortBy=popularity&apiKey=${process.env.REACT_APP_API_KEY}`
+  //   );
+  //   // setContent(data.results);
+  //   console.log(data);
+  //   data.articles.map((article, i) => (
+  //     console.log(article.author)
+  //   ));
+  // };
+
+  // useEffect(() => {
+  //   fetchNews();
+  //   // eslint-disable-next-line
+  // }, []);
+
   return (
     <div className='container'>
       <Switch>
